1d0589db7f8095b61d26c2481f7cfcbc7ae9d02d
Christopher Grainger <chris@amplified.ai>
2023-04-03 22:31:29 +0200

    Add documentation

--- mix.exs
+++ mix.exs
@@ -40,10 +40,10 @@ defmodule Tsne.MixProject do
 
   defp docs do
     [
-      main: "t-SNE",
+      main: "Tsne",
       source_ref: "v#{@version}",
       source_url: @source_url,
-      extras: ["LICENSE"]
+      extras: ["LICENSE", "notebooks/dimensionality_reduction.livemd"]
     ]
   end
 
